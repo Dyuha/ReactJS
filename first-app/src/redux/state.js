bf11ea9bbd4134f17574381f2c2531f47d32e9d9
Dyuha <iamdyuha@tut.by>
2022-11-29 13:51:40 +0300

    dispatch

--- first-app/src/redux/state.js
+++ first-app/src/redux/state.js
@@ -47,21 +47,26 @@ let store = {
   subscribe(observer) {
     this._callSubscriber = observer; // наблюдатель observer
   },
+  _addPost() {
+    const newPost = {
+      id: 8,
+      message: this._state.profilePage.newPostText,
+      likes: 3,
+      dislikes: 11,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  _updateNewPostText(postMessage) {
+    this._state.profilePage.newPostText = postMessage;
+    this._callSubscriber(this._state);
+  },
   dispatch(action) { // action - объект { type: 'ADD-POST', message: 'new message', ... }
-    debugger;
-    if (action.type === 'ADD-POST'){
-      const newPost = {
-        id: 8,
-        message: this._state.profilePage.newPostText,
-        likes: 3,
-        dislikes: 11,
-      };
-      this._state.profilePage.postsData.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubscriber(this._state);
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-      this._state.profilePage.newPostText = action.postMessage;
-      this._callSubscriber(this._state);
+    if (action.type === "ADD-POST") {
+      this._addPost();
+    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+      this._updateNewPostText(action.postMessage);
     }
   },
 };
