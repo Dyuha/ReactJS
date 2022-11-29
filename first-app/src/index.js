b491c7940fb2e1517506166c00837ccfa602c4f8
Dyuha <iamdyuha@tut.by>
2022-11-29 03:51:02 +0300

    store

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,26 +3,24 @@ import reportWebVitals from "./reportWebVitals";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import state, { addPost, subscribe, updateNewPostText } from "./redux/state";
+import store from "./redux/state";
 
 
 // addPost('addPost');
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //костыли (пока что) ререндерим полностью наше дерево с новым state
-export const rerenderEntireTree = (state) => {
+export const rerenderEntireTree = () => {
   root.render(
     <React.StrictMode>
       {/* <App dialogs={state.dialogsData} messages={state.messagesData} posts={state.postData} /> */}
-      <App states={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App states={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>
   );
 }
 
-subscribe(rerenderEntireTree);
-
-rerenderEntireTree(state);
-
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- first-app/src/redux/state.js
+++ first-app/src/redux/state.js
@@ -1,70 +1,69 @@
-// import { rerenderEntireTree } from "../index";
-let rerenderEntireTree = () => {
-  alert('State has been chenched')
-}
-
-
-const state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "My first post!", likes: 12, dislikes: 1 },
-      {
-        id: 2,
-        message: "My second first post!",
-        likes: -30,
-        dislikes: "infinity",
-      },
-      { id: 3, message: "Kak dela?" },
-      { id: 4, message: "Kak dela?" },
-      { id: 5, message: "Kak dela?" },
-      { id: 6, message: "Kak dela?" },
-      { id: 7, message: "Kak dela?" },
-    ],
-    newPostText: 'Some Post Text!!!',
+let store = {
+  _state : {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "My first post!", likes: 12, dislikes: 1 },
+        {
+          id: 2,
+          message: "My second first post!",
+          likes: -30,
+          dislikes: "infinity",
+        },
+        { id: 3, message: "Kak dela?" },
+        { id: 4, message: "Kak dela?" },
+        { id: 5, message: "Kak dela?" },
+        { id: 6, message: "Kak dela?" },
+        { id: 7, message: "Kak dela?" },
+      ],
+      newPostText: 'Some Post Text!!!',
+    },
+    messagesPage: {
+      dialogsData: [
+        { id: 1, name: "Dima" },
+        { id: 2, name: "Egor" },
+        { id: 3, name: "Vlad" },
+        { id: 4, name: "Anton" },
+        { id: 5, name: "Sveta" },
+        { id: 6, name: "Misha" },
+        { id: 7, name: "Lexa" },
+      ],
+      messagesData: [
+        { id: 1, message: "Kak dela?" },
+        { id: 2, message: "Kak dela11212?" },
+        { id: 3, message: "Kak dela?" },
+        { id: 4, message: "Kak dela?" },
+        { id: 5, message: "Kak dela?" },
+        { id: 6, message: "Kak dela?" },
+        { id: 7, message: "Kak dela?" },
+      ],
+    },
   },
-  messagesPage: {
-    dialogsData: [
-      { id: 1, name: "Dima" },
-      { id: 2, name: "Egor" },
-      { id: 3, name: "Vlad" },
-      { id: 4, name: "Anton" },
-      { id: 5, name: "Sveta" },
-      { id: 6, name: "Misha" },
-      { id: 7, name: "Lexa" },
-    ],
-    messagesData: [
-      { id: 1, message: "Kak dela?" },
-      { id: 2, message: "Kak dela11212?" },
-      { id: 3, message: "Kak dela?" },
-      { id: 4, message: "Kak dela?" },
-      { id: 5, message: "Kak dela?" },
-      { id: 6, message: "Kak dela?" },
-      { id: 7, message: "Kak dela?" },
-    ],
+  getState() {
+    return this._state;
   },
-};
-
-window.state = state;
-
-export const addPost = () => {
-  const newPost = {
-    id: 8,
-    message: state.profilePage.newPostText,
-    likes: 3,
-    dislikes: 11,
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
+  _callSubscriber() {
+    alert('State has been chenched')
+  },
+  addPost() {
+    const newPost = {
+      id: 8,
+      message: this._state.profilePage.newPostText,
+      likes: 3,
+      dislikes: 11,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(postMessage) {
+    this._state.profilePage.newPostText = postMessage;
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;         // наблюдатель observer
+  },
 }
 
-export const updateNewPostText = (postMessage) => {
-  state.profilePage.newPostText = postMessage;
-  rerenderEntireTree(state)
-}
+window.store = store;
 
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;         // наблюдатель observer
-}
-
-export default state;
+export default store;
